5044232698fe222198fbeb233ba6d83f0dd2e9b3
lobesteryo <khl97214@naver.com>
2022-02-28 10:04:58 +0900

    결제 적용

--- cookit2/src/main/java/org/iptime/mpage/Controller.java
+++ cookit2/src/main/java/org/iptime/mpage/Controller.java
@@ -12,37 +12,37 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 
-public class Controller extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        System.out.println("--- do ---");
-        action(req, res);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        System.out.println("--- post ---");
-        action(req, res);
-    }
-
-    private void action(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
-        System.out.println("URL :"+req.getRequestURL());
-        System.out.println("URI :"+req.getRequestURI());
-        System.out.println("contecxtPath :"+req.getContextPath());
-        System.out.println("command :"+req.getRequestURI());
-        System.out.println("getServletPath :"+ req.getServletPath());
-
-        UCommand com = null;
-        String viewPage = null;
-
-        if(req.getRequestURI().equals("/aaa.do")){
-            System.out.println("/index.jsp");
-            com = new MainCommand();
-            com.execute(req, res);
-            System.out.println("contecxtPath :"+req.getContextPath());
-            viewPage = "/WEB-INF/view/goods/aaa.jsp";
-        }
+//public class Controller extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//        System.out.println("--- do ---");
+//        action(req, res);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//        System.out.println("--- post ---");
+//        action(req, res);
+//    }
+//
+//    private void action(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//
+//        System.out.println("URL :"+req.getRequestURL());
+//        System.out.println("URI :"+req.getRequestURI());
+//        System.out.println("contecxtPath :"+req.getContextPath());
+//        System.out.println("command :"+req.getRequestURI());
+//        System.out.println("getServletPath :"+ req.getServletPath());
+//
+//        UCommand com = null;
+//        String viewPage = null;
+//
+//        if(req.getRequestURI().equals("/aaa.do")){
+//            System.out.println("/index.jsp");
+//            com = new MainCommand();
+//            com.execute(req, res);
+//            System.out.println("contecxtPath :"+req.getContextPath());
+//            viewPage = "/WEB-INF/view/goods/aaa.jsp";
+//        }
 //        PrintWriter out = res.getWriter();
 //
 //        out.println("<html>");
@@ -57,7 +57,7 @@ public class Controller extends HttpServlet {
 //        out.println("</html>");
 //        out.close();
 
-        req.getRequestDispatcher(viewPage).forward(req, res);
-
-    }
-}
+//        req.getRequestDispatcher(viewPage).forward(req, res);
+//
+//    }
+//}
--- cookit2/src/main/java/org/iptime/mpage/servlet/goods/GoodsMainServlet.java
+++ cookit2/src/main/java/org/iptime/mpage/servlet/goods/GoodsMainServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet("/main")
+@WebServlet("/main")
 public class GoodsMainServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
